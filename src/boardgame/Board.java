0b178b14e7be1c9208f0a49dade252bd9b222573
Marcos Santos <marcosbsantos@gmail.com>
2022-05-29 08:56:21 -0300

    Starting to implement board and pice

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,16 +1,18 @@
 package boardgame;
 
-public class Position {
+public class Board {
 
 	private int rows;
 	private int columns;
+	private Piece[][] pieces;
 	
-	public Position(int rows, int columns) {
+	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
+		pieces = new Piece[rows][columns];
 	}
 
-	public int getRow() {
+	public int getRows() {
 		return rows;
 	}
 
@@ -26,8 +28,5 @@ public class Position {
 		this.columns = columns;
 	}
 	
-	@Override
-	public String toString() {
-		return rows +", " +columns;
-	}
+	
 }
